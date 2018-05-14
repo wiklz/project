e6fe8dc48e9b0c27b0ee9573251abb6916e8e2d9
wiklz <wiklz@ya.ru>
2018-05-14 16:36:49 +0300

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,29 @@
 <template>
   <section class="container">
-    <div>
-      <app-logo/>
-    </div>
+    <h1>NuxtJs</h1>
+    <h3>wiklz</h3>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+
 
 export default {
   components: {
-    AppLogo
+
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 .container {
   min-height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   text-align: center;
 }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
